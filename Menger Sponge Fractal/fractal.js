9db856d188813c12388e78b857734226ea284f41
Shubham Acharya <shubham190398@gmail.com>
2024-07-28 19:53:24 +0530

    Creating a rotating box

--- Menger Sponge Fractal/fractal.js
+++ Menger Sponge Fractal/fractal.js
@@ -1,7 +1,14 @@
+var a = 0.0;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400, WEBGL);
 }
 
 function draw() {
-  background(220);
+  background(50);
+  stroke(255);
+  noFill();
+  rotateX(a);
+  box(200);
+  a += 0.01;
 }
