d59e0279e45ffb3b8a958ca5664a25cf7092079b
Shubham Acharya <shubham190398@gmail.com>
2024-07-28 23:34:35 +0530

    Completing box

--- Menger Sponge Fractal/box.js
+++ Menger Sponge Fractal/box.js
@@ -3,4 +3,34 @@ class Box {
         this.pos = createVector(x, y, z);
         this.size = r;
     }
+
+    generate(){
+        var boxes = []
+        for (let i = -1; i < 2; i++){
+            for (let j = -1; j < 2; j++){
+                for (let k = -1; k < 2; k++){
+                    var sum = abs(i) + abs(j) + abs(k);
+                    var new_size = this.size / 3;
+                    if (sum > 1){
+                        var b = new Box(this.pos.x + x * new_size, this.pos.y + y * new_size, this.pos.z + z * new_size, new_size);
+                        boxes.push(b);
+                    }
+                }
+            }
+        }
+        return boxes;
+    }
+
+    show(){
+        push();
+        translate(this.pos.x, this.pos.y, this.pos.z);
+        noFill();
+        strokeWeight(2);
+        var rValue = map(this.pos.x, -boxWidth_x/2, boxWidth_x/2, 10, 255);
+        var gValue = map(this.pos.y, -boxWidth_y/2, boxWidth_y/2, 10, 255);
+        var bValue = map(this.pos.z, -boxWidth_x/2, boxWidth_x/2, 10, 255);
+        stroke(rValue, gValue, bValue, 255);
+        box(this.r);
+        pop();
+    }
 }
--- Menger Sponge Fractal/fractal.js
+++ Menger Sponge Fractal/fractal.js
@@ -1,4 +1,6 @@
 var a = 0.0;
+var boxWidth_x = 1920;
+var boxWidth_y = 1080;
 
 function setup() {
   createCanvas(400, 400, WEBGL);
