22325c7e3fb3ceb9144312f12645045749659b28
Shubham Acharya <shubham190398@gmail.com>
2024-07-30 02:56:32 +0530

    Giving some initial values to the drop

--- Purple Rain/drop.js
+++ Purple Rain/drop.js
@@ -1,11 +1,14 @@
 class Drop {
-    constructor(x, y){
-        
+    constructor(){
+        this.x = width;
+        this.y = 0;
+        this.speed_y = 1;
     }
     fall(){
-
+        this.y = this.speed_y;
     }
     show(){
-        
+        stroke(138, 43, 226);
+        line(this.x, this.y, this.x, this.y + 10);
     }
 }
