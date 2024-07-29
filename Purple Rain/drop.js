f133503530e9512eddc55cd78799f0775a2b2b95
Shubham Acharya <shubham190398@gmail.com>
2024-07-30 03:51:49 +0530

    Working POV oriented purple rain with weighted values

--- Purple Rain/drop.js
+++ Purple Rain/drop.js
@@ -1,20 +1,26 @@
 class Drop {
     constructor(){
         this.x = random(width);
-        this.y = random(-500, -100);
-        this.speed_y = random(4, 8);
-        this.length = random(10, 20);
+        this.y = random(-800, -100);
+        this.z = random(0, 20);
+        this.speed_y = map(this.z, 0, 10, 3, 15);
+        this.length = map(this.z, 0, 20, 10, 20);
     }
 
     fall(){
         this.y = this.y + this.speed_y;
+        var grav = map(this.z, 0, 20, 0.03, 0.3);
+        this.speed_y += grav;
+
         if (this.y > height){
             this.y = random(-500, -100);
+            this.speed_y = map(this.z, 0, 10, 3, 15);
         }
     }
 
     show(){
-        strokeWeight(2);
+        var weight = map(this.z, 0, 20, 1, 4);
+        strokeWeight(weight);
         stroke(138, 43, 226);
         line(this.x, this.y, this.x, this.y + this.length);
     }
--- Purple Rain/rain.js
+++ Purple Rain/rain.js
@@ -1,15 +1,15 @@
 var drops = []
 
 function setup() {
-  createCanvas(1920, 1080);
-  for (let i = 0; i < 1000; i++){
+  createCanvas(2560, 1440);
+  for (let i = 0; i < 5000; i++){
     drops[i] = new Drop();
   }
 }
 
 function draw() {
   background(230, 230, 250);
-  for (let i = 0; i < 1000; i++){
+  for (let i = 0; i < 5000; i++){
     drops[i].fall();
     drops[i].show();
   }
