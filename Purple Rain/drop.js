9c30f49682b435aed5b336ae2f0a9ca42bb86d86
Shubham Acharya <shubham190398@gmail.com>
2024-07-30 03:42:18 +0530

    Adding variable length and distances

--- Purple Rain/drop.js
+++ Purple Rain/drop.js
@@ -1,20 +1,21 @@
 class Drop {
     constructor(){
         this.x = random(width);
-        this.y = random(-height/2, height/2);
+        this.y = random(-500, -100);
         this.speed_y = random(4, 8);
+        this.length = random(10, 20);
     }
 
     fall(){
         this.y = this.y + this.speed_y;
         if (this.y > height){
-            this.y = random(-height/2, -height/3);
+            this.y = random(-500, -100);
         }
     }
 
     show(){
         strokeWeight(2);
         stroke(138, 43, 226);
-        line(this.x, this.y, this.x, this.y + 15);
+        line(this.x, this.y, this.x, this.y + this.length);
     }
 }
--- Purple Rain/rain.js
+++ Purple Rain/rain.js
@@ -2,14 +2,14 @@ var drops = []
 
 function setup() {
   createCanvas(1920, 1080);
-  for (let i = 0; i < 200; i++){
+  for (let i = 0; i < 1000; i++){
     drops[i] = new Drop();
   }
 }
 
 function draw() {
   background(230, 230, 250);
-  for (let i = 0; i < 200; i++){
+  for (let i = 0; i < 1000; i++){
     drops[i].fall();
     drops[i].show();
   }
