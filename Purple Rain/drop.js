3fd09385a2b42f4954f05a22e7c294f42b81f2a1
Shubham Acharya <shubham190398@gmail.com>
2024-07-30 03:06:46 +0530

    Completing the initial definition with 200 drops and a stroke weight of 2

--- Purple Rain/drop.js
+++ Purple Rain/drop.js
@@ -1,14 +1,20 @@
 class Drop {
     constructor(){
-        this.x = width;
-        this.y = 0;
-        this.speed_y = 1;
+        this.x = random(width);
+        this.y = random(-height/2, height/2);
+        this.speed_y = random(4, 8);
     }
+
     fall(){
-        this.y = this.speed_y;
+        this.y = this.y + this.speed_y;
+        if (this.y > height){
+            this.y = random(-height/2, -height/3);
+        }
     }
+
     show(){
+        strokeWeight(2);
         stroke(138, 43, 226);
-        line(this.x, this.y, this.x, this.y + 10);
+        line(this.x, this.y, this.x, this.y + 15);
     }
 }
