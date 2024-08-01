82bb76773b8236171d97a60500386a23c777657d
Shubham Acharya <shubham190398@gmail.com>
2024-08-02 00:12:55 +0530

    Moving cell

--- Mitosis/cell.js
+++ Mitosis/cell.js
@@ -1,12 +1,15 @@
 class Cell{
     constructor(){
         this.pos = createVector(random(width), random(height));
-        this.radius = 60;
+        this.radius = 20;
     }
 
     move(){
         var vel = createVector(1, 1);
         this.pos.add(vel);
+        if (this.pos.x > width || this.pos.y > height){
+        this.pos = createVector(random(width), random(height));
+        }
     }
 
     draw(){
